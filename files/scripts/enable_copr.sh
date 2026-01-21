27acfd7722f14033571278c4b3e1723cb4c26625
Galih <galih.prastowo@gmail.com>
2026-01-21 20:51:44 +0700

    feat: enforce system-wide dark mode via GSettings and add cursor theme

--- files/scripts/enable_copr.sh
+++ files/scripts/enable_copr.sh
@@ -6,3 +6,4 @@ echo 'Enabling solopasha/hyprland COPR...'
 dnf install -y dnf-plugins-core
 dnf copr enable -y solopasha/hyprland
 dnf copr enable -y tofik/nwg-shell
+dnf copr enable -y peterwu/rendezvous
